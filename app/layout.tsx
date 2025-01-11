5f7585d664c4485cc67664129f60c0dd278664ed
Md Istiak Ahmed <istinishat@gmail.com>
2025-01-11 23:09:25 +0600

    fix bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { GoogleAnalytics } from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
